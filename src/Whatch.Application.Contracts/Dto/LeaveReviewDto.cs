6cabe7997117815cd4f3472bae4160cbeeb53685
Dmitriy <dimden999@gmail.com>
2022-12-13 21:57:24 +0300

    fix

--- src/Whatch.Application.Contracts/Dto/LeaveReviewDto.cs
+++ src/Whatch.Application.Contracts/Dto/LeaveReviewDto.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Whatch.Dto;
 
 public class LeaveReviewDto
 {
-    [Required]
+    [Required] 
     public int FilmId { get; set; }
-    
-    [Required]
-    public Guid UserId { get; set; }
-    
+
     [Required]
     [Range(1, 10)]
     public int Score { get; set; }
     
+    [Required]
     [MaxLength(2048)]
     public string Review { get; set; }
 }
